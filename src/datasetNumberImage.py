80bed59b2c96709d13501d5b7858e659141c2126
m-aryayi <mohammadaryayi@gmail.com>
2024-08-28 07:59:43 +0200

    update image number, N = 498

--- src/datasetNumberImage.py
+++ src/datasetNumberImage.py
@@ -34,7 +34,7 @@ data = {
        'Eyes': 36,
        'Bowel': 18,
        'Breast': 33,
-       'Heart and Blood Vessels': 47, 
+       'Heart and Blood Vessels': 49, 
        'Kidneys and Urinary Tract': 10,
        'Liver': 11, 
        'Lungs': 74,
