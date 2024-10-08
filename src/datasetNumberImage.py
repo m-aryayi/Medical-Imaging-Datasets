4750207ad7f8a17117c30549934e7d017a15e039
m-aryayi <mohammadaryayi@gmail.com>
2024-10-08 07:39:21 +0200

    update image number, N = 550

--- src/datasetNumberImage.py
+++ src/datasetNumberImage.py
@@ -29,7 +29,7 @@ plt.rcParams['text.color']='#333F4B'
 
 
 data = {
-       'Brain': 135,
+       'Brain': 137,
        'Ears, Nose, Teeth, and Throat': 17, 
        'Eyes': 36,
        'Bowel': 19,
