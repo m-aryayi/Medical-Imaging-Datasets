142175f93f1b7876d7c3b96d886beac34e853af0
m-aryayi <mohammadaryayi@gmail.com>
2024-12-06 08:35:24 +0100

    update image number, N = 609

--- src/datasetNumberImage.py
+++ src/datasetNumberImage.py
@@ -45,7 +45,7 @@ data = {
        'Lymph Nodes': 8,
        'Skin': 10,
        'Multi Organs Datasets' : 61,
-       'Animals' : 5,
+       'Animals' : 6,
 }
 
 df = pd.DataFrame.from_dict(data, orient='index', columns=['number'])
