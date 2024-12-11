a1b790f8ec46407835124757a1dffa26650bf413
m-aryayi <mohammadaryayi@gmail.com>
2024-12-11 10:09:41 +0100

    update image number, N = 614

--- src/datasetNumberImage.py
+++ src/datasetNumberImage.py
@@ -44,7 +44,7 @@ data = {
        'Male Reproductive Organs': 17,
        'Lymph Nodes': 8,
        'Skin': 10,
-       'Multi Organs Datasets' : 61,
+       'Multi Organs Datasets' : 62,
        'Animals' : 7,
 }
 
